f3fcd567f3d9b1857b70a8d3e39a5001abbc615a
Darron McIntyre <dmcin003@fiu.edu>
2020-09-24 17:54:05 -0400

    created bootcamp_scripts folder

---
+++ class_scripts/bootcamp_scripts/print_exercise.py
@@ -0,0 +1,49 @@
+
+# this line of code prints out a first line of text
+print("Hi, this is your computer speaking")
+
+# this line of code prints out a second line of text
+print("I can run a lot of python code")
+
+# assigning the variable firstname with 'Paul'
+firstName = "Paul"
+
+# print out contents of the variable
+print(firstName)
+
+
+#integers
+
+print(1+3)
+print (1*3)
+print (1-3)
+
+# addition +
+# subtraction -
+# multiplication *
+# division /
+# exponents **
+
+
+positive_number = 10
+negative_number = -5
+added = positive_number + negative_number
+
+print(added)
+
+
+#floats 
+print( 1.1+3.1)
+print(1.5*2)
+print(1-3.736)
+
+# converting float to integer
+
+print(int(3.0))
+print(int(5.8))
+
+
+#converting integer to float
+print(float(7))
+
+print(4.0 + 4)
