ae79f0dee88d6703e7467a1162dc6de7c416d52f
Darron McIntyre <dmcin003@fiu.edu>
2020-09-25 17:10:45 -0400

    adding string and boolean class scripts

---
+++ class_scripts/bootcamp_scripts/strings_2.py
@@ -0,0 +1,88 @@
+# make a string
+my_string = True
+
+#print(type(my_string))
+
+
+# type conversion -- convert one variable type to another
+a = 2.489261
+
+# str() converts to a string
+
+# assign b to be the string version of a
+# b = str(a)
+# print(a)
+# print(type(a))
+# print(b)
+# print(type(b))
+
+# float() to convert to a float
+#c = '2.0+100'
+#d = float(c)
+
+# print(c)
+# print(type(c))
+# print(d)
+# print(type(d))
+
+# int() to convert to an integer
+# e = 2.9
+# f = int(e)
+
+
+# print(e)
+# print(type(e))
+# print(f)
+# print(type(f))
+
+# convert strings to integers
+#g = '7.0'
+#h = int(g)
+
+# print(g)
+# print(type(g))
+# print(h)
+# print(type(h))
+
+# converting booleans to integers
+# i = False
+# j = int(i)
+
+# print(i)
+# print(type(i))
+# print(j)
+# print(type(j))
+
+# print(True + 2)
+# print(False + 3.5)
+#print(True + False)
+
+print('this is a string')
+a = 'this is a string'
+print(a)
+
+
+a = 2
+b = 3 
+c = 4
+
+print(f'This is a {a} stri{b}ng.{c}')
+
+my_sentence = 'On Monday ' + 'we put'
+print(f'{my_sentence} basketball')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
